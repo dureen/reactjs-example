071beeb003e594a2226720b55b53518708b07488
io-code-dp <dpdevproject@gmail.com>
2022-10-12 01:27:05 +0700

    Changes to be committed: 	modified:   .gitignore 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js 	new file:   src/components/footer.js 	new file:   src/components/header.js 	new file:   src/components/navigation.js 	new file:   src/components/popular.js 	new file:   src/components/trend.js 	modified:   src/index.js

---
+++ src/components/navigation.js
@@ -0,0 +1,18 @@
+import {Navbar, Container,Nav} from "react-bootstrap";
+
+function navigation() {
+    return (
+        <div>
+            <Navbar>
+                <Nav>
+                </Nav>
+            </Navbar>
+            <div>
+                <nav>navbar</nav>
+            </div>
+        </div>
+    )
+}
+
+export default navigation;
+
